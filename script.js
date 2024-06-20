40d55753c090fd350fc10f992972b2fd1ff49bad
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-06-21 02:05:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+const name=document.getElementById("name").value;
+const year=document.getElementById("year").value;
+const btn=document.getElementById("button");
+const h3=document.getElementById("url");
+
+function changeURL() {
+	const queryString="";
+	// if(name.textContent!=""){
+	// 	queryString=`?name=${name.textContent}`;
+	// }
+	// if(year.textContent!=""){
+	// 	queryString=`&year=${year.textContent}`;
+	// }
+	h3.textContent=`https://localhost:8080/${queryString}`
+}
+
+
