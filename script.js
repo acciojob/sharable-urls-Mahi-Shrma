dda11a82ba74a3bb642e8a3a790d087b9401098a
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-08-02 00:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // your code here
 const name=document.getElementById("name").value;
-const year=document.getElementById("year").value;
+const year=document.getElementById("year").;
 const btn=document.getElementById("button");
-const h3=document.getElementById("url");
+const url=document.getElementById("url");
 
-function changeURL() {
-	const queryString="";
-	// if(name.textContent!=""){
-	// 	queryString=`?name=${name.textContent}`;
-	// }
-	// if(year.textContent!=""){
-	// 	queryString=`&year=${year.textContent}`;
-	// }
-	h3.textContent=`https://localhost:8080/${queryString}`
-}
+btn.addEventListener('click',function(){
+	url.innerHTML = url.innerHTML + `name= ${name.value}&year= ${year.value}`
+})
 
 
