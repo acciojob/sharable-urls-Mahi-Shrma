c689c1e2160618a784675ddce986576cfe81433b
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-08-02 00:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btn=document.getElementById("button");
 const url=document.getElementById("url");
 
 btn.addEventListener('click',function(){
-	url.innerHTML = url.innerHTML + `name= ${name.value}&year= ${year.value}`
+	url.innerHTML = url.innerHTML + `?name= ${name.value}&year= ${year.value}`
 })
 
 
